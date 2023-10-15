a461194bc8aaa67ffa369e311c1967806171a5d4
arnadeglenn <125632656+arnadeglenn@users.noreply.github.com>
2023-10-14 23:56:18 -0700

    Create dialog to add new project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const projectDialog = document.getElementById("project-addition");
+// const addProject = document.getElementById('add-project-button');
+
+// addProject.addEventListener('click', (e) => {
+//     projectDialog.showModal();
+// })
+
